1c88c7991de2b9d009c1de2e3f8773bc4ac30e1a
ShinnelElisa <nellie_martini@yahoo.com>
2024-04-22 22:40:01 -0400

    updated to add a button

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './card.styles.css';
 
+export const Card = (props) => {
+    const [string, setString] = useState('');
 
-export const Card = (props) => (
-    <div className='card-container'>
-        <h2> {props.monster.top_work} </h2>
-        <img alt="Add this piece to your colloction today!" src={`https://i.pinimg.com/originals/a9/88/bc/a988bccb5dfb2dfb3510af13511e5d3e.jpg`} />
-        <h5> Copies available for purchase </h5>
-        <p>{props.monster.work_count}</p>
-    </div>
-)
+    return (
+        <div className='card-container'>
+            <h2> {props.monster.top_work} </h2>
+            <img alt="Add this piece to your collection today!" src={`https://i.pinimg.com/originals/a9/88/bc/a988bccb5dfb2dfb3510af13511e5d3e.jpg`} />
+            <h5> Copies available for purchase: {props.monster.work_count} </h5>
+            <button className='custom-button' onClick={() => {
+                setString('In store purchase only');
+            }}> Get yours </button>
+            <p>{string}</p>
+        </div>
+    );
+}
